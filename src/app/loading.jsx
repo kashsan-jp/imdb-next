c89051bddc31679e1998a8d0e157528ae9067b1d
“kashsan-jp” <kashsan_jp@yahoo.co.jp>
2024-05-09 17:36:50 +0900

    add loading effect

---
+++ src/app/loading.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Loading() {
+  return (
+    <div className='flex justify-center mt-1'>
+        <img className="h-55" src='spinner.svg' alt='loading...'/>
+    </div>
+  );
+}
