2b01cdd4de75f024fc7135713e7342f91677edd5
“kashsan-jp” <kashsan_jp@yahoo.co.jp>
2024-05-13 14:47:51 +0900

    add card component

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+    // remotePatterns: [
+    //     {
+    //         protocol: 'https',
+    //         hostname: 'image.tmdb.org',
+    //         pathname: '**',
+    //     },
+    // ],
+    images: {
+        domains: ['image.tmdb.org'],
+    }
+};
+//module.exports = nextConfig;
 export default nextConfig;
